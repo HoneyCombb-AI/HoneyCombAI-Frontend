82918f8870107894e2338b546b8f8cbd4f9de090
cernoalpha <mirshahzadmubeen1@gmail.com>
2025-09-26 04:23:50 +0530

    Logo motherfucker

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -3,7 +3,7 @@
 import { Loading } from "@/components/loading";
 import { SimpleHeader } from "@/components/SimpleHeader";
 import { useAuth } from "@/lib/auth-context";
-import Image from "next/image";
+import { AnimatedLogo } from "@/components/AnimatedLogo";
 import { redirect } from "next/navigation";
 
 export default function RootLayout({
@@ -31,11 +31,9 @@ export default function RootLayout({
         <div className="hidden md:flex flex-col justify-between bg-gradient-to-r from-amber-300 via-amber-200 to-yellow-100/90">
           <div className="w-full flex-1 flex flex-col justify-center items-center px-16">
             <div className="max-w-2xl ml-12">
-              <Image
-                src="/iconLogin.png"
-                alt="Honeycomb AI Logo"
-                width={84}
-                height={48}
+              <AnimatedLogo
+                width={102}
+                height={96}
                 className="mb-6"
               />
               <h1 className="text-5xl font-black text-black leading-tight mb-6">
