cb8e84290186100cb00ccd8543f580afee465912
cernoalpha <mirshahzadmubeen1@gmail.com>
2025-09-30 02:14:23 +0530

    fixx

--- app/messages/layout.tsx
+++ app/messages/layout.tsx
@@ -1,6 +1,5 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-import Header from "@/components/dashboard/Header";
 
 export default function RootLayout({
   children,
