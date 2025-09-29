2a8986ef9b44c8d8efd4c1ebe34b11add5e2ac91
cernoalpha <mirshahzadmubeen1@gmail.com>
2025-09-29 09:08:45 +0530

    Cal Link Update General but will be fixed later.

--- components/Landing/DesktopLanding.tsx
+++ components/Landing/DesktopLanding.tsx
@@ -14,12 +14,12 @@ import { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
 
 export default function DesktopLanding() {
-    useEffect(() => {
-        (async function () {
-          const cal = await getCalApi({ namespace: "30min" });
-          cal("ui", { hideEventTypeDetails: false, layout: "month_view" });
-        })();
-      }, []);
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({"namespace":"30min"});
+      cal("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
+    })();
+  }, [])
       
   return (
     <>
